684b836eed1396cf7d6e80260a73b3c698b4ab76
Sline-X <kipa.maksim99@gmail.com>
2024-01-23 01:59:54 +0300

    singleton realization

--- src/Zandstra/Chapter9/Singleton/Preferences.php
+++ src/Zandstra/Chapter9/Singleton/Preferences.php
@@ -6,8 +6,19 @@ class Preferences
 {
     private array $props = [];
 
+    private static Preferences $instance;
+
     private function __construct()
     {
+    }
+
+    public static function getInstance(): Preferences
+    {
+        if (empty(self::$instance)){
+            self::$instance = new Preferences();
+        }
+
+        return self::$instance;
     }
 
     public function setProperty(string $key, string $val): void
