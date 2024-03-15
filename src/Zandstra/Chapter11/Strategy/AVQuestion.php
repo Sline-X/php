b013253474e6d6e0ef5ff822bf5e78097ec30ce1
Sline-X <kipa.maksim99@gmail.com>
2024-03-15 18:44:00 +0300

    feat(chapter11): add file pattern Strategy

---
+++ src/Zandstra/Chapter11/Strategy/AVQuestion.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Main\Zandstra\Chapter11\Strategy;
+
+use Main\Zandstra\Chapter11\Strategy\Question;
+
+class AVQuestion extends Question
+{
+    // Обработка вопроса в мультимедийном виде
+}
